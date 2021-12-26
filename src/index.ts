0f0df2f4bdc838931d7c9c400893309c3b7eff4e
lvneto <lauro.neto.1995@gmail.com>
2021-12-25 16:29:27 -0800

    local passport done

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,9 @@ const port = 3001
 const app = express()
 
 import routes from './routes/routes'
-import clientsRoutes from './routes/clients.routes'
-import productsRoutes from './routes/products.routes'
-import stockRoutes from './routes/stock.routes'
-import usersRoutes from './routes/users.routes'
 
 app.use(express.json())
 app.use(routes)
-app.use('/products', productsRoutes)
-app.use('/clients', clientsRoutes)
-app.use('/users', usersRoutes)
-app.use('/stock', stockRoutes)
-
 
 app.listen(port, () =>
   console.log(`
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,21 @@
 import express from 'express'
+import clientsRoutes from './clients.routes'
+import productsRoutes from './products.routes'
+import stockRoutes from './stock.routes'
+import usersRoutes from './users.routes'
 const app = express()
 
 const router = express.Router()
 
 app.use(express.json())
-router.get('/', async (req, res) => {
-  res.json("You can access routes:/users /clients /products and /stock")
+
+router.post('/login', async (req, res) => {
+  res.json("Login Router")
 })
+
+router.use('/products', productsRoutes)
+router.use('/clients', clientsRoutes)
+router.use('/users', usersRoutes)
+router.use('/stock', stockRoutes)
 
 export default router;
