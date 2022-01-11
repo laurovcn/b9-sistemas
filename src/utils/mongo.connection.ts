09bd67c0a39585e8c085ff48be755082e9ad660d
lvneto <lauro.neto.1995@gmail.com>
2022-01-11 20:20:04 -0300

    add mongo db

---
+++ src/utils/mongo.connection.ts
@@ -0,0 +1,9 @@
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://dev:dev@cluster0.to5qx.mongodb.net/b9system?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
