6b46a80d88f48dab3fcf3ac106951e0663e47bbe
lvneto <lauro.neto.1995@gmail.com>
2022-01-12 21:51:06 -0300

    working at crud mongo db

--- src/utils/mongo.connection.ts
+++ src/utils/mongo.connection.ts
@@ -2,8 +2,14 @@
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://dev:dev@cluster0.to5qx.mongodb.net/b9system?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {  
-  const testQuery = client.db("b9system").collection("init");
-  testQuery.find({})  
+client.connect(() => {  
+  const query = client.db("b9system").collection("init");
+  query.collection.insertOne({
+    "name": "test",
+  }),
+  query.collection.find({}).toArray(function(err: any, result: any) {
+    if (err) throw err;
+    console.log(result);
+  })
   client.close();
 });
