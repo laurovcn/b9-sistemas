fc1f57ea9196dbee9e15fd98d5cafb9cd9d9de97
lvneto <lauro.neto.1995@Gmail.com>
2022-01-14 15:01:06 -0300

    remove unecessary imports

--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import UsersController from '../../controllers/Users/users.controller'
 
 export const usersRouter = express.Router()
@@ -7,11 +7,11 @@ usersRouter.use(express.json())
 
 const controller = new UsersController()
 
-usersRouter.get('/',  controller.findAll.bind(controller))
+usersRouter.get('/', controller.findAll.bind(controller))
 
-usersRouter.post('/',  controller.create.bind(controller))
+usersRouter.post('/', controller.create.bind(controller))
 
-usersRouter.put('/:id',  controller.update.bind(controller))
+usersRouter.put('/:id', controller.update.bind(controller))
 
-usersRouter.delete('/:id',  controller.delete.bind(controller))
+usersRouter.delete('/:id', controller.delete.bind(controller))
 
